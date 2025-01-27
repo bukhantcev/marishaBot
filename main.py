12a5dcbf1f04a2dab6df307b758fae8d2f16e2ce
buha <bukhantcev@mail.ru>
2025-01-27 10:06:39 +0300

    second

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ from aiogram.utils import executor
 
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 client = OpenAI(api_key=OPENAI_API_KEY)
-def get_ai_response(user_input):
+def get_ai_response(user_input, role:str):
     try:
         # Отправляем запрос к OpenAI API
         response = client.chat.completions.create(model="gpt-3.5-turbo",  # Используемая модель
         messages=[
-            {"role": "system", "content": "Ты будешь меня хвалить. Обращайся ко мне как к девушке в женском роде. Называй меня Мариша, Маришенька, Мариночка, Солнышко, Солнце, Маришечка"},  # Системное сообщение для настройки поведения
+            {"role": "system", "content": role},  # Системное сообщение для настройки поведения
             {"role": "user", "content": user_input}  # Сообщение от пользователя
         ],
         max_tokens=1000,  # Максимальное количество токенов в ответе
@@ -43,10 +43,18 @@ dp = Dispatcher(bot)
 # Обработчик текстовых сообщений (Эхо-бот)
 @dp.message_handler()
 async def echo_message(message: Message):
-    print(message)
-    await bot.send_message(chat_id=404354012, text=message.text)
+
     if message.chat.id == 404354012 or message.chat.id == 857601623:
-        await message.answer(get_ai_response(message.text))
+        if message.chat.id == 857601623:
+            role = ("Ты будешь меня хвалить. Обращайся ко мне как к девушке в женском роде. Называй меня Мариша, Маришенька, "
+                    "Мариночка, Солнышко, Солнце, Маришечка")
+            await bot.send_message(chat_id=404354012, text=message.text)
+            answer = get_ai_response(message.text, role=role)
+            await bot.send_message(chat_id=404354012, text=answer)
+        if message.chat.id == 404354012:
+            role = ("Ты программист и умный помощник")
+            answer = get_ai_response(message.text, role=role)
+        await message.answer(answer)
     else:
         await message.answer("Уходи, я не хочу с тобой разговаривать!")
 if __name__ == "__main__":
